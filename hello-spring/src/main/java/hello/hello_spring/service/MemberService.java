1767754741b87ae8a6e89b575caa94f20e0e3b61
dlrbdjs <gon2yq@naver.com>
2024-12-31 21:13:00 +0900

    Create client service

---
+++ hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,39 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원가입
+    public Long join(Member member) {
+
+        validateDuplicateMember(member);    // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // 같은 이름이 있는 중복 회원X
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {     // if null 이렇게 안하고 Optional 로 감싼 걸 ifPresent, 꺼내고 싶으면 get()으로 꺼내기
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
