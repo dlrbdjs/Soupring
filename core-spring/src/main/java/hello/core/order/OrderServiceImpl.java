f314ab158e6c5fbffa9f65cfc92ddef2a2f85db6
dlrbdjs <gon2yq@naver.com>
2025-02-19 15:45:13 +0900

    으아아아아아아아악

---
+++ core-spring/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,30 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { // fix or rate 들어오는 걸로
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);            // 회원정보 조회
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 할인 정책에 회원 넘기기
+
+        return new Order(memberId, itemName, itemPrice, discountPrice); // 값 받아서 Order 생성
+    }
+}
