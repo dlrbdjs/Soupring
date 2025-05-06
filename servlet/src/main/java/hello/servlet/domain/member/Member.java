45cc82985c38c80c1f228e37a5bd27e5ad7c6d02
Gyueon Lee <gon2yq@naver.com>
2025-05-07 00:37:00 +0900

    Member management with servlet and java

--- servlet/src/main/java/hello/servlet/domain/member/Member.java
+++ servlet/src/main/java/hello/servlet/domain/member/Member.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 public class Member {
 
     private Long id;
-    private String name;
+    private String username;
     private int age;
 
     public Member() {
@@ -16,7 +16,7 @@ public class Member {
     }
 
     public Member(String name, int age) {
-        this.name = name;
+        this.username = name;
         this.age = age;
     }
 }
