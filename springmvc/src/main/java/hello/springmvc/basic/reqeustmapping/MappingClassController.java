19f53fded576347ae2bf60547ede5f67b9ebffab
Gyueon Lee <gon2yq@naver.com>
2025-08-18 18:12:10 +0900

    MappingClassController

---
+++ springmvc/src/main/java/hello/springmvc/basic/reqeustmapping/MappingClassController.java
@@ -0,0 +1,32 @@
+package hello.springmvc.basic.reqeustmapping;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mapping/users")
+public class MappingClassController {
+
+    @GetMapping()
+    public String user() {
+        return "get users";
+    }
+
+    @PostMapping()
+    public String addUser() {
+        return "post user";
+    }
+
+    @GetMapping("/{userId}")
+    public String findUser(
+            @PathVariable String userId
+    ) {
+        return "get userId = " + userId;
+    }
+
+    @DeleteMapping("/{userId}")
+    public String deleteUser(
+            @PathVariable String userId
+    ) {
+        return "delete userId = " + userId;
+    }
+}
