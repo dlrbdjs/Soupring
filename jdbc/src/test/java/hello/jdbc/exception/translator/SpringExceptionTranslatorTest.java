d5589569202b2f96fd03d57c86e90670294c3dd0
Gyueon Lee <gon2yq@naver.com>
2025-10-01 22:01:36 +0900

    Add exceptionTranslator

--- jdbc/src/test/java/hello/jdbc/exception/translator/SpringExceptionTranslatorTest.java
+++ jdbc/src/test/java/hello/jdbc/exception/translator/SpringExceptionTranslatorTest.java
@@ -4,7 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
 
 import javax.sql.DataSource;
 
@@ -13,6 +16,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
 public class SpringExceptionTranslatorTest {
@@ -26,17 +30,37 @@ public class SpringExceptionTranslatorTest {
 
     @Test
     void sqlExceptionErrorCode() {
-        String sql = "select bad grammer";
+        String sql = "select bad grammar";
 
         try {
             Connection con = dataSource.getConnection();
             PreparedStatement stmt = con.prepareStatement(sql);
             stmt.executeQuery();
         } catch (SQLException e) {
-            Assertions.assertThat(e.getErrorCode()).isEqualTo(42122);
+            assertThat(e.getErrorCode()).isEqualTo(42122);
             int errorCode = e.getErrorCode();
             log.info("errorCode={}", errorCode);
             log.info("error", e);
+        }
+    }
+
+    @Test
+    void exceptionTranslator() {
+        String sql = "select bad grammar";
+
+        try {
+            Connection con = dataSource.getConnection();
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.executeQuery();
+        } catch (SQLException e) {
+            assertThat(e.getErrorCode()).isEqualTo(42122);
+
+            SQLErrorCodeSQLExceptionTranslator exTranslator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
+
+            // BadSqlGrammarException
+            DataAccessException resultEx = exTranslator.translate("select", sql, e);
+            log.info("resultEx", resultEx);
+            assertThat(resultEx.getClass()).isEqualTo(BadSqlGrammarException.class);
         }
     }
 }
